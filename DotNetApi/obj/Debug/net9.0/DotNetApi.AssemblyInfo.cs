660455517d8ed241926157282fd2b61037525e29
Ruslan <ruslanryscovbluerex@gmail.com>
2026-02-18 02:08:57 +0100

    fix: update assembly informational version and cache files for DotNetApi and DotNetApiAot

--- DotNetApi/obj/Debug/net9.0/DotNetApi.AssemblyInfo.cs
+++ DotNetApi/obj/Debug/net9.0/DotNetApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5142dcee84595ea48fc85d84f0a918f4cf9edd2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c213ef6d76a779fce612ccb78a4100275ecc7329")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DotNetApiAot/obj/Debug/net9.0/DotNetApiAot.AssemblyInfo.cs
+++ DotNetApiAot/obj/Debug/net9.0/DotNetApiAot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetApiAot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5142dcee84595ea48fc85d84f0a918f4cf9edd2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c213ef6d76a779fce612ccb78a4100275ecc7329")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetApiAot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetApiAot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
